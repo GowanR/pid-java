3a3871b1e36619a76fb3c5702f088faa298aecca
Gowan Rowland <gowan.rowland@gmail.com>
2016-09-22 16:11:19 -0700

    filtering begins

--- PID.java
+++ PID.java
@@ -14,10 +14,12 @@ public class PID {
 	private double derivative;
 	private signal functions;
 	private double output;
+	private double m_setpoint;
 	public PID ( signal functions ){
 		this.functions = functions;
 		integral = 0;
 		setpoint = 0;
+		m_setpoint = functions.getValue();
 	}
 	public void set_setpoint( double s ){
 		setpoint = s;
@@ -32,6 +34,22 @@ public class PID {
 	 */
 	static double lerp( double a, double b, double t ){
 		return a + (b - a) * t;
+	}
+	/*
+	 * Experimental update function for filtering pv, co, and ramping sp.
+	 * 	TODO:
+	 * 		second derivative
+	 * 		co filter
+	 * 		pv filter
+	 * 		sp ramp
+	 */
+	public void xupdate( double dt ){
+		m_setpoint = lerp( setpoint, m_setpoint, dt);
+		error =  m_setpoint - functions.getValue();
+		integral += ( Ki * error * dt );
+		derivative = ( prevError - error ) / dt;
+		prevError = error;
+		output = ( Kp * error ) + ( integral ) + ( Kd * derivative );
 	}
 	public void update( double dt ){
 		error = setpoint - functions.getValue();
